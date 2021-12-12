d87a9582221039dcecce4161b5b8cc208b0fdb03
Grazi Felix <grazielafelix2001@gmail.com>
2021-12-12 19:52:41 -0300

    Função excluir dados do MySQLworkbanch

--- Clinicas_Medicas_Interface.py
+++ Clinicas_Medicas_Interface.py
@@ -2,20 +2,12 @@ from tkinter import *
 import tkinter.messagebox as MessageBox
 import mysql.connector as mysqlcn
 
-
-def janela2():
-    index.destroy()
-    janela2 = Tk()
-    janela2.geometry('500x600')
-    janela2.title("janela 2")
-    janela2.mainloop()
-
 def inserir():
     CodCli = e_codcli.get()
     NomeCli = e_NomeCli.get()
     Endereco = e_Endereco.get()
     Telefone = e_Telefone.get()
-    Email = e_Telefone.get()
+    Email = e_Email.get()
     if (CodCli == '' or NomeCli == "" or Endereco == '' or Telefone == "" or Email == ""):
         MessageBox.showinfo("Insira todas as informações para poder enviar.")
     else:
@@ -23,12 +15,41 @@ def inserir():
                                         database='clinicas_medicas')
         cursor = db_connection.cursor()
         sql = 'insert into CLINICA (CodCli, NomeCli, Endereco, Telefone, Email) VALUES (%s, %s, %s, %s, %s)'
-        values = (5, 'Clinica Omega', 'Rua Camelia, 66, Recife-PE', '36656998', 'clinicaomega@gmail.com')
+        values = (CodCli, NomeCli, Endereco,  Telefone, Email)
         cursor.execute(sql, values)
-        print(cursor.rowcount, "record inserted.")
+
+        #limpando inputs
+        e_codcli.delete(0, 'end')
+        e_NomeCli.delete(0, 'end')
+        e_Endereco.delete(0, 'end')
+        e_Telefone.delete(0, 'end')
+        e_Email.delete(0, 'end')
+
+        MessageBox.showinfo(cursor.rowcount, "record inserted.")
         cursor.close()
         db_connection.commit()
         db_connection.close()
+
+def delete():
+    if (e_codcli.get()==""):
+        MessageBox.showinfo("Adicione o codigo para clinica para excluir")
+    else:
+        db_connection = mysqlcn.connect(host='127.0.0.1', user='root', password='Choich@n2208', database='clinicas_medicas')
+        cursor = db_connection.cursor()
+        cursor.execute("DELETE FROM clinica WHERE CodCli='"+e_codcli.get()+"'")
+        cursor.execute("commit")
+        e_codcli.delete(0, 'end')
+        e_NomeCli.delete(0, 'end')
+        e_Endereco.delete(0, 'end')
+        e_Telefone.delete(0, 'end')
+        e_Email.delete(0, 'end')
+
+        MessageBox.showinfo(cursor.rowcount, "Delete Sucessfully.")
+        cursor.close()
+        db_connection.commit()
+        db_connection.close()
+
+
 
 #INDEXSCREEN
 index = Tk()
@@ -79,9 +100,17 @@ e_Email.place(x=200, y=50)
 e_Email.grid(column=3, row=4, padx=10, pady=10)
 
 #botão
-inserirButton = Button(index, text='Inserir', font=('italic', 10), bg='white', command=janela2)
+inserirButton = Button(index, text='Inserir', font=('italic', 10), bg='white', command=inserir)
 inserirButton.place(x=50, y=20)
 inserirButton.grid(column=2, row=6, padx=10, pady=10)
+
+visualizarButton = Button(index, text='Atualizar', font=('italic', 10), bg='white', command=atualizar)
+visualizarButton.place(x=50, y=20)
+visualizarButton.grid(column=3, row=6, padx=10, pady=10)
+
+deleteButton = Button(index, text='Deletar', font=('italic', 10), bg='white', command=delete)
+deleteButton.place(x=50, y=20)
+deleteButton.grid(column=4, row=6, padx=10, pady=10)
 
 index.mainloop()
 
